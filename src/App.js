0c675f5d6ed40bf7ba077707b296d11b62152420
preetsimon <77456490+preetsimon@users.noreply.github.com>
2023-01-15 08:06:39 +0000

    next react topic

--- src/App.js
+++ src/App.js
@@ -2,33 +2,10 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const decreaseCount = () => {
-    setCount(count-1);
-  }
-
-  const increaseCount = () => {
-    setCount(count+1);
-  }
 
   return (
     <div className="App">
- <h1>Welcome to my counter</h1>
- <p>The count is:{count}</p>
- <button onClick={()=>decreaseCount()}> - </button>
- <button onClick={()=>increaseCount()}> + </button>
-
-  <br></br>
-  <p>
-  State: correct way of defining variables in React.
-Allows us to have some memory inside the component that is local to that component.
-<br/>
-State is non-persistent: Refreshing the web page -> states are lost.
-
-  </p>
-
-
+<h1>Todo</h1>
     </div>
   );
 }
